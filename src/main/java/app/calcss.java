2ddc891563fa48f2db73aee3d7d2ad3a03f12c0c
MailMuradov <muradovmail@gmail.com>
2020-02-10 19:38:11 +0400

    calcSS

---
+++ src/main/java/app/calcss.java
@@ -0,0 +1,10 @@
+package app;
+
+public class calcss {
+    public static void main(String[] args) {
+        int numberOne = 1;
+        int numberTwo = 2;
+        System.out.println("Choose a number " + numberOne + " or " + numberTwo);
+
+    }
+}
